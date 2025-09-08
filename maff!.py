4e0649c0224c4ddf93c1d701dce314c3249ec164
JonThornThin <ctw22.jonathan.gawlowski@corketb.ie>
2025-09-08 13:55:54 +0100

    Add files via upload

---
+++ maff!.py
@@ -0,0 +1,34 @@
+#maffffff!!!!!!!!!!1!1!!1!1
+
+action=input("whatcha doin (+, -, *, /, **) ")
+num1=input("whats the first number? ")
+num1=float(num1)
+num2=input("with? ")
+num2=float(num2)
+if action=="+":
+    end=num1 + num2
+elif action=="-"
+    end=num1-num2
+elif action=="*":
+    end=num1*num2
+elif action=="/":
+    end=num1/num2
+elif action=="**":
+    end=num1**num2
+print (end)
+
+#great heavans! this works exactly as i want it!!!!!!!!!!
+
+#print(pow(3, abs(-3)))
+#print(4^3) ???
+
+#runEnd=0
+#val1=10
+#val2=14
+#val3=6
+#runEnd=runEnd+val1
+#runEnd=runEnd+val2
+#runEnd=runEnd+val3
+#print(runEnd)
+
+#what else could a running integer be used for, maybe letting that calc of mine do more than two nums at once
