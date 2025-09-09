48368fd13314d3ff3d3d64f725f9c80d67933923
JonThornThin <ctw22.jonathan.gawlowski@corketb.ie>
2025-09-09 17:00:59 +0100

    Update maff!.py

--- maff!.py
+++ maff!.py
@@ -6,18 +6,19 @@ num2=input("whats the second? ")
 num2=float(num2)
 if act=="+":
     end=num1+num2
-if act=="-":
+elif act=="-":
     end=num1-num2
-if act=="*":
+elif act=="*":
     end=num1*num2
-if act=="/":
+elif act=="/":
     end=num1/num2
-if act=="**":
+elif act=="**":
     end=pow(num1,num2)
 print(num1,act,num2,"=",end)
 roundEnd=round(end, 2)
 print("rounded:", roundEnd)
 
 #great heavans! this works exactly as i want it!!!!!!!!!!
+
 
 #ash jamu ts jon thorn thin
