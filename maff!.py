0d75af8e464fbe34dc097dacf595227c1665673c
JonThornThin <ctw22.jonathan.gawlowski@corketb.ie>
2025-09-09 16:58:22 +0100

    Add files via upload

--- maff!.py
+++ maff!.py
@@ -1,43 +1,23 @@
 #maffffff!!!!!!!!!!1!1!!1!1
-
 num1=input("whats the first number? ")
-act=input("whatcha doin? (+, -, *, /, **) ")
 num1=float(num1)
+act=input("whatcha doin? (+, -, *, /, **) ")
 num2=input("whats the second? ")
 num2=float(num2)
 if act=="+":
-    end=num1 + num2
-elif act=="-":
+    end=num1+num2
+if act=="-":
     end=num1-num2
-elif act=="*":
+if act=="*":
     end=num1*num2
-elif act=="/":
+if act=="/":
     end=num1/num2
-elif act=="**":
-    end=pow(num1, num2) #same as num1**num2
-print(end)
-end=float(end)
-round(end,4)
-print(end)
-abs(end) #?
+if act=="**":
+    end=pow(num1,num2)
+print(num1,act,num2,"=",end)
+roundEnd=round(end, 2)
+print("rounded:", roundEnd)
 
 #great heavans! this works exactly as i want it!!!!!!!!!!
 
-#print(pow(3, abs(-3)))
-#print(4^3) ???
-
-#runEnd=0
-#val1=10
-#val2=14
-#val3=6
-#runEnd=runEnd+val1
-#runEnd=runEnd+val2
-#runEnd=runEnd+val3
-#print(runEnd)
-
-#what else could a running integer be used for, maybe letting that calc of mine do more than two nums at once
-
-a=1.23456789
-round (a, 3)
-print(a)
 #ash jamu ts jon thorn thin
