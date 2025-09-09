e37229f4203c1e36b0a382d6636b33777075f59b
JonThornThin <ctw22.jonathan.gawlowski@corketb.ie>
2025-09-09 17:28:03 +0100

    Add files via upload

---
+++ billy.py
@@ -0,0 +1,15 @@
+# starter program lesson 1
+#name=input("what be thy name?")
+#print("We want to know if you like programming!")
+#print()
+#answer = input("Do you like programming " + name + "?" )
+#print("Great! You said ", answer, "!")
+#print("Let's learn some Python today")
+#the = are a standin for commas,
+#harke ye need a plus to mix a var and sting it seems
+
+name=input("what be thy name?")
+age=input("how many years have thee?")
+food=input("what have thou eaten last?")
+age=int(age)
+print("greetings mx.",name,"i hope thou hafth enjoyt thine",food,"thou shall becometh",age+1,"in time coming, i vvish thee a happy early birthday")
