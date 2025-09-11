5de02235eb769d2b9b3ff9bfe445241b8d466495
JonThornThin <ctw22.jonathan.gawlowski@corketb.ie>
2025-09-11 20:24:43 +0100

    Add files via upload

--- billy.py
+++ billy.py
@@ -5,12 +5,57 @@
 #answer = input("Do you like programming " + name + "?" )
 #print("Great! You said ", answer, "!")
 #print("Let's learn some Python today")
-#the = are a standin for commas,
-#harke ye need a plus to mix a var and sting it seems
+#the + are a standin for commas,
+#harke ye need a plus to mix a var and sting it seems, me znaara
 
-name=input("what be thy name?")
-age=input("how many years have thee?")
-food=input("what have thou eaten last?")
-age=int(age)
+#name=input("what be thy name?")
+#age=input("how many years have thee?")
+#food=input("what have thou eaten last?")
+#age=int(age)
+#print("greetings mx.",name,"i hope thou hafth enjoyt thine",food,",","thou vvilt becometh",age+1,"in time coming, i vvish thee a happy early birthday")
+#15 - 130 aaa jan dzoverr dzuvrujna hhalaa angdze
 
-print("greetings mx.",name,"i hope thou hafth enjoyt thine",food,"thou vvilt becometh",age+1,"in time coming, i vvish thee a happy early birthday")
+#character="j"
+#ascii=ord(character)
+#print(type(ascii))
+#print(ascii)
+
+# starter program week 2 - perhaps this could be a task
+
+#def conversation():
+#    print("Welcome to my conversation program")
+#    print()
+#    print("Do you like cycling? Answer \"yes\" or something else")
+#    answer = input()
+#    if answer=="yes":
+#        print("That's good, you will get very fit")
+#    else:
+#        print("Perhaps you like some other sport. ")
+#    print()
+#    print("Goodbye")
+
+#conversation()
+#def says what the conversation is like var=var, naming the def and then () to indicate function makes it activate at any time 
+#1st colon is for telling the computer to read the stuff after it to define the function, the else: defines a new function within the previous
+#= is for vars and == is of ifs
+
+#def conv2():
+#    print("doch ryee liken feed?")
+#    ans2=input("yay or nay?")
+#    if ans2==("yay"):
+#        print("dentvvi allen?")
+#        print("heve nees dee")
+#    elif ans2=="nay":
+#        print("death be upon thee")
+        
+#conv2()
+
+def qs():
+    ansqs=input("how many there be of the greater luminants")
+    ansqs=int(ansqs)
+    if ansqs==7:
+        print("indubitably")
+    else:
+        print("wrong, how could u possible be this foolish")
+
+qs()
